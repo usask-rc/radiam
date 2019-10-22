f8398a5cf29aef5e72e35f7b4a59d6e9a85b728b
Todd Trann <todd.trann@usask.ca>
2019-10-22 13:37:51 -0600

    Add padding to logo

--- gui/src/layout/RadiamLogo.jsx
+++ gui/src/layout/RadiamLogo.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 const RadiamLogo = props => (
     <img
+        id="radiamlogo"
         src={require("../_assets/RadiamLogoClear_NoBorder.png")}
         alt="Radiam"
         height={70}
