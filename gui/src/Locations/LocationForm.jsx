7d7df489b4e991f2be687c0637f24676303bd263
Adam McKenzie <adam.mckenzie@usask.ca>
2019-10-23 16:30:10 +0000

    Handle an empty geo json value for locations gracefully

--- gui/src/Locations/LocationForm.jsx
+++ gui/src/Locations/LocationForm.jsx
@@ -18,6 +18,7 @@ import { withStyles } from '@material-ui/styles';
 import { TextField, Button, ExpansionPanel, ExpansionPanelSummary, ExpansionPanelDetails, Typography, Grid } from '@material-ui/core';
 import { ExpandMore } from '@material-ui/icons';
 import { FormDataConsumer } from 'ra-core';
+import get from 'lodash/get';
 
 const validateHostname = required('en.validate.locations.host_name');
 const validateLocationType = required('en.validate.locations.location_type');
@@ -50,7 +51,7 @@ class LocationForm extends Component {
   }
 
   componentDidMount() {
-    this.setState({ geoText: JSON.stringify(this.state.geo.geojson.features, null, 2) });
+    this.setState({ geoText: JSON.stringify(get(this, "state.geo.geojson.features", {}), null, 2) });
   }
 
   geoDataCallback = geo => {
