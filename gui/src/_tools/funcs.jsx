bb31a21a3108ba79abc22d282b6a25f7a977fbaa
jmloewen <jonathan.loewen@computecanada.ca>
2019-10-31 11:43:11 -0700

    resolved a bug with redirect on project create.  Must now test Datasets and Locations with this fix.

--- gui/src/_tools/funcs.jsx
+++ gui/src/_tools/funcs.jsx
@@ -343,7 +343,7 @@ function updateObjectWithGeo(formData, geo, props){
 }
 
 //TODO: When creating Projects, there is a failure somewhere here.
-export function createObjectWithGeo(formData, geo, props, redirect){
+export function createObjectWithGeo(formData, geo, props){
   let headers = new Headers({ "Content-Type": "application/json" });
   const token = localStorage.getItem(Constants.WEBTOKEN);
   
@@ -400,7 +400,7 @@ export function createObjectWithGeo(formData, geo, props, redirect){
         })
         .then(data => {  
             console.log("data after update is: ", data)
-            props.history.push(`${redirect}/`)
+            props.history.push(`/${props.resource}`)
         })
       }
       )
