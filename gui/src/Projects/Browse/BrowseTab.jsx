643b0a904986a4be03e20d055194a144e05173be
jmloewen <jonathan.loewen@computecanada.ca>
2019-11-13 15:22:33 -0800

    small changes to how state loading works in the browse view

--- gui/src/Projects/Browse/BrowseTab.jsx
+++ gui/src/Projects/Browse/BrowseTab.jsx
@@ -33,28 +33,27 @@ function BrowseTab({ projectID, classes }) {
         };
         getProjectData(params).then(data => {
 
-          if (data && data.files.length > 0){ //else there are no project files
-
-            console.log("data is: ", data)
-
+          if (data && data.files && data.files.length > 0){ //else there are no project files
           let folderItem = { id: `${data.files[0].id}`, key: `${data.files[0].key}`, path_parent: data.files[0].path_parent, path: data.files[0].path }
 
           setListOfRootPaths([folderItem])
-          setStatus({loading: false})
+          setStatus({loading: false, error: false})
+          }
+          else{
+            setStatus({loading: false, error: "No Files were found in this project"})
           }
         })
       }
       else{
         if (_isMounted){      
           setListOfRootPaths(data)
-          setStatus({loading: false})
+          setStatus({loading: false, error: false})
         }
         return data
       }
 
     }).catch(err => {
       setStatus({ loading: false, error: err })
-      console.error(err)
     })
 
     //if we unmount, lock out the component from being able to use the state
@@ -65,11 +64,11 @@ function BrowseTab({ projectID, classes }) {
 
   return (
     <div className={classes.main}>
-    {status && status.error ? 
-      <Typography>{`Error loading File Data.  Please Refresh the page and try again.`}</Typography>
-      : status && status.loading ? (
-        <Typography>{`Loading File Data...`}</Typography>
-      ) : listOfRootPaths && listOfRootPaths.length > 0 &&
+    {!status.loading && status.error ? 
+      <Typography>{status.error}</Typography>
+      : status.loading ? (
+        <Typography>{`Loading...`}</Typography>
+      ) : listOfRootPaths.length > 0 &&
           listOfRootPaths.map(item => {
             return <FolderView
               expanded={"true"}
