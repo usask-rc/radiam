918b4f49061441c664a03a9664785dfeb6496376
Jonathan Loewen <jono@Jonathans-MacBook-Pro.local>
2019-12-14 17:08:52 -0800

    bumped version

--- gui/src/Projects/Files/FilesTab.jsx
+++ gui/src/Projects/Files/FilesTab.jsx
@@ -116,11 +116,11 @@ function FilesTab({ projectID, classes, translate, ...props }) {
             onChange={e => setSort(e.target.value)}
           >
             {/* TODO: Translate has troubles with this component.  How to fix?  Probably through HOC*/}
-            <MenuItem value={Constants.model_fields.NAME}>File Name</MenuItem>
-            <MenuItem value={Constants.model_fields.INDEXED_DATE}>Indexed On</MenuItem>
-            <MenuItem value={Constants.model_fields.LAST_MODIFIED}>Last Modified</MenuItem>
-            <MenuItem value={Constants.model_fields.FILESIZE}>Filesize</MenuItem>
-            <MenuItem value={Constants.model_fields.LAST_ACCESS}>Last Accessed</MenuItem>
+            <MenuItem value={Constants.model_fields.NAME}>{`File Name`}</MenuItem>
+            <MenuItem value={Constants.model_fields.INDEXED_DATE}>{`Indexed On`}</MenuItem>
+            <MenuItem value={Constants.model_fields.LAST_MODIFIED}>{`Last Modified`}</MenuItem>
+            <MenuItem value={Constants.model_fields.FILESIZE}>{`Filesize`}</MenuItem>
+            <MenuItem value={Constants.model_fields.LAST_ACCESS}>{`Last Accessed`}</MenuItem>
           </Select>
 
           <div className={classes.sortDisplay}>
