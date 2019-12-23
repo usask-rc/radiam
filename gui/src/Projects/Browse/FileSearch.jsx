26000cd016369718f82e1ecf79e67fbeaf08fe3f
Jonathan Loewen <jono@jonathans-mbp.gateway.mts.net>
2019-12-23 10:53:18 -0600

    changes to header display for file search

--- gui/src/Projects/Browse/FileSearch.jsx
+++ gui/src/Projects/Browse/FileSearch.jsx
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from 'react'
 import compose from 'recompose/compose';
 import withStyles from '@material-ui/core/styles/withStyles';
-import { Table, TableHead, TableCell, TableRow } from '@material-ui/core';
+import { Table, TableHead, TableCell, TableRow, Typography } from '@material-ui/core';
 
 const styles = theme => {
     root: {
@@ -15,24 +15,27 @@ const FileSearch = ({files, ...props}) => {
     return(<Table>
         <TableHead>
             <TableCell>
+                <Typography>{`Filename`}</Typography>
             </TableCell>
 
             <TableCell>
+                <Typography>{`Filepath`}</Typography>
             </TableCell>
 
             <TableCell>
+                <Typography>{`Filesize`}</Typography>
             </TableCell>
 
             <TableCell>
+                <Typography>{`Last Updated`}</Typography>
             </TableCell>
         </TableHead>
     {files.map(file => {
         return (<TableRow>
             <TableCell>{file.name}</TableCell>
-            <TableCell></TableCell>
-
-            <TableCell></TableCell>
-            <TableCell></TableCell>
+            <TableCell>{file.path}</TableCell>
+            <TableCell>{file.size}</TableCell>
+            <TableCell>{file.last_udpated}</TableCell>
 
         </TableRow>)
 
