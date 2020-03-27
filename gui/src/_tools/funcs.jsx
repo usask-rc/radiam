4157768dcc32d12b3115c4d534bda4923788eca9
Jonathan Loewen <jono@Jonathans-MacBook-Pro.local>
2020-03-27 16:03:51 -0700

    call bug is fixed

--- gui/src/_tools/funcs.jsx
+++ gui/src/_tools/funcs.jsx
@@ -13,7 +13,6 @@ const dataProvider = radiamRestProvider(getAPIEndpoint(), httpClient);
 
 //returns the endpoint set in constants
 export function getAPIEndpoint() {
-  return `https://dev2.radiam.ca/api`
   return `/${API_ENDPOINT}`;
 }
 
